34f1cc2dba983c6da611a36228a42aa6e70fc457
Rafael Stefani Baptista <rafael.stefani.baptista@gmail.com>
2019-04-28 16:57:27 -0300

    First fixed version.

--- src/gnome_drive/Cliente.java
+++ src/gnome_drive/Cliente.java
@@ -2,112 +2,106 @@ package gnome_drive;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class Cliente {
-	public final static int FILE_SIZE = Integer.MAX_VALUE;
 
-	public static void main(String argv[]) throws Exception {
-		// Cria a conexao com o servidor e as streams de input e output
-		Socket socketCliente = new Socket("127.0.0.1", 6789);
-		DataOutputStream paraServidor = new DataOutputStream(socketCliente.getOutputStream());
-		DataInputStream doServidor = new DataInputStream(socketCliente.getInputStream());
-		Scanner scan = new Scanner(System.in);
-		int opcao = -1;
-		while (opcao != 2) {			
-			opcao = menu();
-			switch (opcao) {
-			case 0:
-				paraServidor.writeUTF("" + 1);
-				enviaArquivo(paraServidor, selectFile());
-				break;
-			case 1:
-				paraServidor.writeUTF("" + 2);
-				 String resposta = doServidor.readUTF();
-				if (resposta.equals("null")) {
-					System.out.println("Nao existem arquivos no servidor para serem transferidos!");
-				} else {
-                                        String respostas[] = resposta.split(";");
-					int opcao2 = JOptionPane.showOptionDialog(null, "Selecione um arquivo",
-                                    "Gnome Drive",
-                                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, respostas, respostas[0]);
-					paraServidor.writeUTF("" + opcao2+1);
-					recebeArquivo(socketCliente);
-				}
-				break;
-			}
+    public static void main(String[] argv) throws Exception {
+        // Cria a conexao com o servidor e as streams de input e output
+        Socket socketCliente = new Socket("127.0.0.1", 6789);
+        DataOutputStream paraServidor = new DataOutputStream(socketCliente.getOutputStream());
+        DataInputStream doServidor = new DataInputStream(socketCliente.getInputStream());
+        int opcao = -1;
+        while (opcao != 2) {
+            opcao = menu();
+            switch (opcao) {
+                case 0:
+                    paraServidor.writeInt(1);
+                    enviaArquivo(paraServidor, selectFile());
+                    break;
+                case 1:
+                    paraServidor.writeInt(2);
+                    String resposta = doServidor.readUTF();
+                    if (resposta.equals("null")) {
+                        System.out.println("Nao existem arquivos no servidor para serem transferidos!");
+                    } else {
+                        String[] respostas = resposta.split(";");
+                        int opcao2 = JOptionPane.showOptionDialog(null, "Selecione um arquivo",
+                                "Gnome Drive",
+                                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, respostas, respostas[0]);
+                        paraServidor.writeInt(opcao2 + 1);
+                        try {
+                            recebeArquivo(doServidor);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    break;
+            }
+        }
+        // Fecha o socket e as streams
+        paraServidor.close();
+        doServidor.close();
+        socketCliente.close();
+    }
 
-		}
-		// ENVIA MENSAGEM PARA ENCERRAR A CONEXAO COM O SERVIDOR
-		paraServidor.writeUTF("" + 1234);
-		// Fecha o socket e as streams
-		scan.close();
-		paraServidor.close();
-		doServidor.close();
-		socketCliente.close();
-	}
+    private static void enviaArquivo(DataOutputStream paraServidor, File file) throws IOException {
+        InputStream in = new FileInputStream(file);
+        paraServidor.writeUTF(file.getName());
+        paraServidor.writeLong(file.length());
+        byte[] bytes = new byte[16 * 1024];
+        int count;
+        System.out.println("Enviando arquivo para o servidor...");
+        while ((count = in.read(bytes)) > 0) {
+            System.out.println("Enviando: " + count);
+            paraServidor.write(bytes, 0, count);
+        }
+        in.close();
+    }
 
-	public static void enviaArquivo(DataOutputStream paraServidor, File file) throws IOException {
-		InputStream in = new FileInputStream(file);
-		paraServidor.writeUTF(file.getName());
-		byte[] bytes = new byte[16 * 1024];
-		int count;
-		while ((count = in.read(bytes)) > 0) {
-			paraServidor.write(bytes, 0, count);
-		}
-		// Envia o caracter para sinalizar que terminou de enviar o arquivo
-		paraServidor.write('#');
-		in.close();
-	}
+    private static void recebeArquivo(DataInputStream doServidor) throws Exception {
+        File arquivo = selectFile();
+        OutputStream out = new FileOutputStream(arquivo);
+        long quantidadeDeBytesRestantes = doServidor.readLong();
 
-	private static void recebeArquivo(Socket socketCliente) throws IOException {
-		InputStream doServidor = socketCliente.getInputStream();
-		File arquivo = selectFile();
-		OutputStream out = new FileOutputStream(arquivo);
-		byte[] bytes = new byte[16 * 1024];
-		int count;
-		while ((count = doServidor.read(bytes)) > 0) {
-			System.out.println(count);
-			if (count == 1 && bytes[0] == '#') {
-				break;
-			} 
-			if(bytes[count-1] == '#') {
-				break;
-			}
-			else {
-				out.write(bytes, 0, count);
-			}
-		}
-		System.out.println("Arquivo recebido do servidor!");
-		out.close();
-	}
+        byte[] bytes = new byte[16 * 1024];
+        int count;
+        while ((count = doServidor.read(bytes)) > 0) {
+            System.out.println(count);
+            out.write(bytes, 0, count);
+            quantidadeDeBytesRestantes -= count;
+            if (quantidadeDeBytesRestantes <= 0) {
+                System.out.println("Arquivo recebido do servidor!");
+                break;
+            }
+        }
+        out.close();
+    }
 
-	public static File selectFile() throws FileNotFoundException {
+    private static File selectFile() throws Exception {
+        JFileChooser fileChooser = new JFileChooser();
 
-		JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            String filename = fileChooser.getSelectedFile().getPath();
+            System.out.println(filename);
+            return new File(filename);
+        } else if (result == JFileChooser.CANCEL_OPTION) {
+            JOptionPane.showMessageDialog(null, "Nenhum arquivo foi selecionado!");
+            throw new Exception("Nenhum arquivo foi selecionado!");
+        } else if (result == JFileChooser.ERROR_OPTION) {
+            JOptionPane.showMessageDialog(null, "Erro ao selecionar o arquivo!");
+            throw new Exception("JFileChooser: Erro ao selecionar arquivo");
+        }
+        throw new Exception("Erro ao selecionar arquivo");
+    }
 
-		int result = fileChooser.showOpenDialog(null);
-		if (result == JFileChooser.APPROVE_OPTION) {
-			String filename = fileChooser.getSelectedFile().getPath();
-			System.out.println(filename);
-			File file = new File(filename);
-			// Get the size of the file
-			return file;
-		} else if (result == JFileChooser.CANCEL_OPTION) {
-			JOptionPane.showMessageDialog(null, "Nenhum arquivo foi selecionado!");
-		} else if (result == JFileChooser.ERROR_OPTION) {
-			JOptionPane.showMessageDialog(null, "Erro ao selecionar o arquivo!");
-		}
-		return null;
-	}
-
-	private static int menu() {
-            String[] opcoes = {"Enviar Arquivo para o servidor", "Baixar arquivo do servidor", "Sair"};
+    private static int menu() {
+        String[] opcoes = {"Enviar Arquivo para o servidor", "Baixar arquivo do servidor", "Sair"};
         return JOptionPane.showOptionDialog(null, "Selecione uma opção",
                 "Gnome Drive",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
-	}
+    }
 }
--- src/gnome_drive/Servidor.java
+++ src/gnome_drive/Servidor.java
@@ -5,112 +5,110 @@ import java.net.*;
 import java.util.ArrayList;
 
 class Servidor extends Thread {
-	public final static int FILE_SIZE = Integer.MAX_VALUE;
-	Socket socketCliente;
-	static ArrayList<File> arquivos = new ArrayList<File>();
-	static int numCliente = 0;
+    private Socket socketCliente;
+    private static ArrayList<File> arquivos = new ArrayList<>();
+    private static int numCliente = 0;
 
-	public Servidor(Socket cliente) {
-		this.socketCliente = cliente;
-	}
+    private Servidor(Socket cliente) {
+        this.socketCliente = cliente;
+    }
 
-	public static void main(String argv[]) throws Exception {
-		ServerSocket servidor = new ServerSocket(6789);
-		System.out.println("Server na porta " + servidor.getLocalPort());
-		while (true) {
-			Socket cliente = servidor.accept();
-			numCliente += 1;
-			Thread thread = new Thread(new Servidor(cliente));
-			thread.start();
-			if (!thread.isAlive()) {
-				servidor.close();
-			}
-		}
+    @SuppressWarnings("InfiniteLoopStatement")
+    public static void main(String[] argv) throws Exception {
+        ServerSocket servidor = new ServerSocket(6789);
+        System.out.println("Server na porta " + servidor.getLocalPort());
+        while (true) {
+            Socket cliente = servidor.accept();
+            numCliente += 1;
+            Thread thread = new Thread(new Servidor(cliente));
+            thread.start();
+            if (!thread.isAlive()) {
+                servidor.close();
+            }
+        }
 
-	}
+    }
 
-	@Override
-	public void run() {
-		System.out.println(
-				"Cliente " + Servidor.numCliente + " Conectado: " + socketCliente.getInetAddress().getHostAddress());
-		try {
-			// Criando streams de input e output
-			DataOutputStream paraCliente = new DataOutputStream(socketCliente.getOutputStream());
-			DataInputStream doCliente = new DataInputStream(socketCliente.getInputStream());
+    @Override
+    public void run() {
+        System.out.println(
+                "Cliente " + Servidor.numCliente + " Conectado: " + socketCliente.getInetAddress().getHostAddress());
+        try {
+            // Criando streams de input e output
+            DataOutputStream paraCliente = new DataOutputStream(socketCliente.getOutputStream());
+            DataInputStream doCliente = new DataInputStream(socketCliente.getInputStream());
 
-			// Recebe a opcao escolhida pelo cliente
-			int opcao = 1;
-			while (opcao > 0 && opcao < 3) {
-				opcao = Integer.parseInt(doCliente.readUTF());
-				switch (opcao) {
-				case 1:
-						recebeArquivo(doCliente);
-					break;
-				case 2:
-					if (arquivos.isEmpty()) {
-						paraCliente.writeUTF("null");
-					} else {
-						String nomeArquivos = "";
-						for (int i = 0; i < arquivos.size(); i++) {
-							nomeArquivos += arquivos.get(i).getName() + ";";
-						}
-						paraCliente.writeUTF(nomeArquivos);
-						int opcao2 = Integer.parseInt(doCliente.readUTF());
-						System.out.println(opcao2);
-						if (opcao2 > 0 && opcao2 <= arquivos.size()) {
-							enviaArquivo(paraCliente, opcao2 - 1);
-						}
-					}
-					break;
-				default:
+            // Recebe a opcao escolhida pelo cliente
+            int opcao = 1;
+            while (opcao > 0 && opcao < 3) {
+                opcao = doCliente.readInt();
+                switch (opcao) {
+                    case 1:
+                        recebeArquivo(doCliente);
+                        break;
+                    case 2:
+                        if (arquivos.isEmpty()) {
+                            paraCliente.writeUTF("null");
+                        } else {
+                            StringBuilder nomeArquivos = new StringBuilder();
+                            for (File arquivo : arquivos) {
+                                nomeArquivos.append(arquivo.getName()).append(";");
+                            }
+                            paraCliente.writeUTF(nomeArquivos.toString());
+                            int opcao2 = doCliente.readInt();
+                            System.out.println(opcao2);
+                            if (opcao2 > 0 && opcao2 <= arquivos.size()) {
+                                enviaArquivo(paraCliente, opcao2 - 1);
+                            }
+                        }
+                        break;
+                }
+            }
+            System.out.println("Encerrando conexao  com o cliente!");
+            doCliente.close();
+            paraCliente.close();
+            socketCliente.close();
+            Servidor.numCliente -= 1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-					break;
-				}
-			}
-			System.out.println("Encerrando conexao  com o cliente!");
-			doCliente.close();
-			paraCliente.close();
-			socketCliente.close();
-			Servidor.numCliente -= 1;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    }
 
-	}
+    private static void enviaArquivo(DataOutputStream paraCliente, int numArquivo) throws IOException {
+        File file = arquivos.get(numArquivo);
+        InputStream in = new FileInputStream(file);
+        paraCliente.writeLong(file.length());
+        byte[] bytes = new byte[16 * 1024];
+        int count;
+        System.out.println("Enviando arquivo para o cliente...");
+        while ((count = in.read(bytes)) > 0) {
+            System.out.println("Enviando: " + count);
+            paraCliente.write(bytes, 0, count);
+        }
+        in.close();
+        System.out.println("Enviado!");
+    }
 
-	public static void enviaArquivo(DataOutputStream paraCliente, int numArquivo) throws IOException {
-		InputStream in = new FileInputStream(arquivos.get(numArquivo));
-		byte[] bytes = new byte[16 * 1024];
-		int count;
-		System.out.println("Enviando arquivo para o cliente...");
-		while ((count = in.read(bytes)) > 0) {
-			paraCliente.write(bytes, 0, count);
-		}
-		// Envia o caracter para sinalizar que terminou de enviar o arquivo
-		paraCliente.write('#');
-		in.close();
-		System.out.println("Enviado!");
-	}
+    private void recebeArquivo(DataInputStream input) throws IOException {
+        InputStream doCliente = socketCliente.getInputStream();
+        String fileName = input.readUTF();
+        long quantidadeDeBytesRestantes = input.readLong();
+        File file = new File(fileName);
+        arquivos.add(file);
+        OutputStream out = new FileOutputStream(file);
 
-	private void recebeArquivo(DataInputStream input) throws IOException {
-		InputStream doCliente = socketCliente.getInputStream();
-		arquivos.add(new File(input.readUTF()));
-		OutputStream out = new FileOutputStream(arquivos.get(arquivos.size() - 1));
-		byte[] bytes = new byte[16 * 1024];
-		int count;
-		while ((count = doCliente.read(bytes)) > 0) {
-			if (count == 1 && bytes[0] == '#') {
-				System.out.println("Arquivo recebido do cliente!");
-				break;
-			}
-			else if (bytes[count - 1] == '#') {
-				System.out.println("Arquivo recebido do cliente!");
-				break;
-			} else {
-				out.write(bytes, 0, count);
-			}
-		}
-		out.close();
-	}
+        byte[] bytes = new byte[16 * 1024];
+        int count;
+        while ((count = doCliente.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
+            quantidadeDeBytesRestantes -= count;
+            if (quantidadeDeBytesRestantes <= 0) {
+                System.out.println("Arquivo recebido do cliente!");
+                break;
+            }
+        }
+        out.close();
+    }
 
 }
